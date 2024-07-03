0d71564401c5d4a65331bdeb5bee9c57c025132d
jigolaurito93 <jigolaurito93@yahoo.com>
2024-07-03 08:51:31 -0500

    changed bg color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-[#0e0b23]`}>
         <Navbar />
         {children}
         <Footer />

211ae7e496677fc7bb51886e3a9330cfc7151289
jigolaurito93 <jigolaurito93@yahoo.com>
2024-07-03 09:14:31 -0500

    conditional rendering to display admin and login logout for navbar

--- src/components/navbar/Links.tsx
+++ src/components/navbar/Links.tsx
@@ -1,36 +1,50 @@
 import Link from "next/link";
 import NavLink from "./navLink";
 
-const Links = () => {
-  interface LinkType {
-    title: string;
-    path: string;
-  }
+interface LinkType {
+  title: string;
+  path: string;
+}
 
-  const links: LinkType[] = [
-    {
-      title: "Home",
-      path: "/",
-    },
-    {
-      title: "About",
-      path: "/about",
-    },
-    {
-      title: "Contact",
-      path: "/contact",
-    },
-    {
-      title: "Blog",
-      path: "/blog",
-    },
-  ];
+const links: LinkType[] = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+];
+
+const Links = () => {
+  const session = true;
+  const isAdmin = true;
+  console.log("hello");
 
   return (
     <div className="flex gap-10">
       {links.map((link) => (
         <NavLink item={link} key={link.title} />
       ))}
+      {session ? (
+        <>
+          {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+          <button className="bg-white px-3 rounded-sm">Logout</button>
+        </>
+      ) : (
+        <>
+          <button className="bg-white rounded-sm px-3">Login</button>
+        </>
+      )}
     </div>
   );
 };
